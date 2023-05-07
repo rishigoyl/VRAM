cca473ae2d9eb71626495bb5984bdcb206397d46
Rishi Goyal <118888273+rishigoyl@users.noreply.github.com>
2023-05-07 14:50:47 +0530

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -2,6 +2,6 @@
 cd ~/.config && mkdir notify && cd notify && touch provider-config.yaml
 echo "telegram:" >> provider-config.yaml
 echo "  - id: \"tel\"" >> provider-config.yaml
-echo "    telegram_api_key: \"6276093405:AAG3YdW8Ieu4VWXBm1IUDUZW1gE3lfORorI\"" >> provider-config.yaml
-echo "    telegram_chat_id: \"1248713511\"" >> provider-config.yaml
+echo "    telegram_api_key: \"\"" >> provider-config.yaml
+echo "    telegram_chat_id: \"\"" >> provider-config.yaml
 echo "    telegram_format: \"{{data}}\"" >> provider-config.yaml
